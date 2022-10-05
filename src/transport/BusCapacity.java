02f25beccfc75875b4fcf1de8ef68469accf873d
Spolpinka <a421243266@gmail.com>
2022-10-05 18:38:00 +0300

    реализованы перечисления для всех видов транспорта

--- src/transport/BusCapacity.java
+++ src/transport/BusCapacity.java
@@ -1,15 +1,18 @@
 package transport;
 
 public enum BusCapacity {
-    ESPECIALLY_SMALL ("супер малая"),// (up to 10 seats),
-    SMALL ("малая"),// (up to 25),
-    MEDIUM ("средняя"),// (40-50),
-    LARGE ("большая"),// (60-80),
-    ESPECIALLY_LARGE ("супер большая");// (100-120 seats)
+    ESPECIALLY_SMALL ("супер малая", "up to 10 seats"),
+    SMALL ("малая", "up to 25"),
+    MEDIUM ("средняя", "40-50"),
+    LARGE ("большая", "60-80"),
+    ESPECIALLY_LARGE ("супер большая","100-120 seats");
 
     private String name;
 
-    private BusCapacity (String name){
+    private String description;
+
+    private BusCapacity (String name, String description){
         this.name = name;
+        this.description = description;
     }
 }
